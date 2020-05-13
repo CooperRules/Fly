dde45862fa52c56d2c0081d8aec655b2423b0c6f
CooperRules <33394304+CooperRules@users.noreply.github.com>
2020-05-13 18:47:19 -0400

    Add files via upload

---
+++ Plugin/src/net/maplemc/cooper/ConfigManager.java
@@ -0,0 +1,5 @@
+package net.maplemc.cooper;
+
+public class ConfigManager {
+
+}
---
+++ Plugin/src/net/maplemc/cooper/Main.java
@@ -0,0 +1,66 @@
+package net.maplemc.cooper;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+	FileConfiguration config = getConfig();
+	@Override
+	public void onEnable() {
+		this.getCommand("fly").setExecutor(new Main());
+		this.saveDefaultConfig();
+        config.addDefault("message on", "apesex");
+        config.addDefault("message off", "off");
+        config.options().copyDefaults(true);
+        saveConfig();
+        	
+        	
+        	
+        }
+
+	
+	@Override
+	public void onDisable() {
+		
+		
+	}
+	public boolean onCommand(CommandSender commandsend, Command fly, String simplefirstword, String[] arg0) {
+		if (commandsend instanceof Player) {
+			Player player = (Player) commandsend;
+			if (player.hasPermission("simple.fly")) {
+
+				if (player.isFlying() == true) {
+					player.setAllowFlight(false);
+					player.setFlying(false);
+					player.sendMessage(config.getString("message on"));
+
+				}
+				else {
+					player.setAllowFlight(true);			
+					player.setFlying(true);
+					player.sendMessage(config.getString("message off"));
+
+				}
+
+
+
+
+
+			}
+			else {
+				player.sendMessage(ChatColor.DARK_RED + "You do not have permission for this command!");
+			}
+		}
+		if (commandsend instanceof ConsoleCommandSender) {
+			System.out.println("You need to be a player to use this command!");
+
+		}
+		return true;
+	}
+}
