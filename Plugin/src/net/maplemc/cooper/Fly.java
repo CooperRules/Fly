c1d01053805279038fbc940fa307ebcc80c10516
coope <coope@ape>
2020-05-13 21:22:58 -0400

    Fixed sloppy code, should work now.

---
+++ Plugin/src/net/maplemc/cooper/Fly.java
@@ -0,0 +1,45 @@
+package net.maplemc.cooper;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class Fly {
+
+	public boolean onCommand(CommandSender commandsend, Command fly, String simplefirstword, String[] arg0) {
+		if (commandsend instanceof Player) {
+			Player player = (Player) commandsend;
+			if (player.hasPermission("simple.fly")) {
+
+				if (player.isFlying() == true) {
+					player.setAllowFlight(false);
+					player.setFlying(false);
+					player.sendMessage(ChatColor.DARK_RED + "Fly on");
+
+				}
+				else {
+					player.setAllowFlight(true);			
+					player.setFlying(true);
+					player.sendMessage(ChatColor.DARK_RED + "Fly off");
+
+				}
+
+
+
+
+
+			}
+			else {
+				player.sendMessage(ChatColor.DARK_RED + "You do not have permission for this command!");
+			}
+		}
+		if (commandsend instanceof ConsoleCommandSender) {
+			System.out.println("You need to be a player to use this command!");
+
+		}
+		return false;
+	}
+
+}
--- Plugin/src/net/maplemc/cooper/Main.java
+++ Plugin/src/net/maplemc/cooper/Main.java
@@ -12,15 +12,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
-	FileConfiguration config = getConfig();
 	@Override
 	public void onEnable() {
 		this.getCommand("fly").setExecutor(new Main());
-		this.saveDefaultConfig();
-        config.addDefault("message on", "apesex");
-        config.addDefault("message off", "off");
-        config.options().copyDefaults(true);
-        saveConfig();
+
         Logger logger = this.getLogger();
         
         new UpdateChecker(this, 78792).getVersion(version -> {
@@ -38,40 +33,9 @@ public class Main extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onDisable() {
-		
+		System.out.println("Shutting Down.");
 		
 	}
-	public boolean onCommand(CommandSender commandsend, Command fly, String simplefirstword, String[] arg0) {
-		if (commandsend instanceof Player) {
-			Player player = (Player) commandsend;
-			if (player.hasPermission("simple.fly")) {
 
-				if (player.isFlying() == true) {
-					player.setAllowFlight(false);
-					player.setFlying(false);
-					player.sendMessage(config.getString("message on")));
-
-				}
-				else {
-					player.setAllowFlight(true);			
-					player.setFlying(true);
-					player.sendMessage(config.getString("message off"));
-
-				}
-
-
-
-
-
-			}
-			else {
-				player.sendMessage(ChatColor.DARK_RED + "You do not have permission for this command!");
-			}
-		}
-		if (commandsend instanceof ConsoleCommandSender) {
-			System.out.println("You need to be a player to use this command!");
-
-		}
-		return true;
-	}
+	
 }
